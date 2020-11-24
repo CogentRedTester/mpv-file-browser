CogentRedTester/mpv-file-browser

commit c169330ad5ae078b5a17cedb574bc1a788bf350e

folders are now sorted alphabetically (#7)

file-browser.lua
@@ -49,6 +49,10 @@ local keybinds = {
     {'Ctrl+r', 'reload', function() cache={}; update() end, {}}
 }
 
+function sort(t)
+    table.sort(t, function(a,b) return a:lower() < b:lower() end)
+end
+
 --splits the string into a table on the semicolons
 function setup_root()
     root = {}
@@ -190,6 +194,8 @@ function update_list(reload)
     end
 
     state.root = false
+
+    sort(list)
     for i,item in ipairs(list) do
         if (state.prev_directory == item) then
             state.selected = i
@@ -200,8 +206,10 @@ function update_list(reload)
 
     --array concatenation taken from https://stackoverflow.com/a/15278426
     local list2 = utils.readdir(state.directory, 'files')
-    for i = 1,#list2 do
-        list[#list+1] = {name = list2[i], type = 'file'}
+    sort(list2)
+    local num_folders = #list
+    for i,v in ipairs(list2) do
+        list[num_folders + i] = {name = v, type = 'file'}
     end
     msg.debug('load time: ' ..mp.get_time() - t)
 

commit b8983e01b140680c7569f530404e8cdbdccbf1c0

add better support for selecting prev dir when moving up
This commit means that addons also benfit from this feature, and it
fixes a bug introduced by c169330.

file-browser.lua
@@ -64,7 +64,7 @@ local extensions = nil
 local state = {
     directory = nil,
     selection = {},
-    prev_directory = nil,
+    prev_directory = "",
     current_file = {
         directory = nil,
         name = nil
@@ -191,6 +191,21 @@ local function filter(t)
     end
 end
 
+--scans the list for the folder that the script just moved out of
+--must be run after the sort
+local function select_prev_directory()
+    if state.prev_directory:find(state.directory, 1, true) ~= 1 then return end
+
+    local i = 1
+    while (list.list[i] and list.list[i].type == "dir") do
+        if state.prev_directory:find(state.directory..list.list[i].name) then
+            list.selected = i
+            break
+        end
+        i = i+1
+    end
+end
+
 --splits the string into a table on the semicolons
 local function setup_root()
     root = {}
@@ -241,15 +256,11 @@ local function goto_root()
 
     --if moving to root from one of the connected locations,
     --then select that location
-    for i,item in ipairs(list.list) do
-        if (state.prev_directory == item.name) then
-            list.selected = i
-            break
-        end
-    end
+    state.directory = ""
+    select_prev_directory()
+
     state.parser = ""
     state.prev_directory = ""
-    state.directory = ""
     cache = {}
     state.selection = {}
     update_header()
@@ -272,7 +283,6 @@ local function update_local_list()
     --sorts folders and formats them into the list of directories
     for i=1, #list1 do
         local item = list1[i]
-        if (state.prev_directory == state.directory..item..'/') then list.selected = i end
 
         --filters hidden dot directories for linux
         if o.filter_dot_dirs and item:find('%.') == 1 then goto continue end
@@ -298,8 +308,8 @@ local function update_local_list()
 
         ::continue::
     end
-
     sort(list.list)
+    select_prev_directory()
 
     --saves the latest directory at the top of the stack
     cache[#cache+1] = {directory = state.directory, table = list.list}
@@ -594,6 +604,7 @@ mp.register_script_message('update-list-callback', function(json)
     list.list = utils.parse_json(json)
     if o.filter_files or o.filter_dot_dirs then filter(list.list) end
     sort(list.list)
+    select_prev_directory()
 
     --setting up the cache stuff
     cache[#cache+1] = {directory = state.directory, table = list.list}