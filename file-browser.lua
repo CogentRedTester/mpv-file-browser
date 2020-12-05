CogentRedTester/mpv-file-browser

commit b2f1222dea2a7d1b03e059f0b8e08b022fb258d4

custom-dir-loading: significantly reduced recursion for local files

file-browser.lua
@@ -489,15 +489,12 @@ local directory_parser = {
 
     --continue with the next directory in the queue/stack
     continue = function(this)
-        if not this.stack[1] then
-            if this.queue[1] then
-                this:setup_parse(this.queue[1])
-                table.remove(this.queue, 1)
-                this:open_directory()
-            end
-            return
+        if this.stack[1] then this:open_directory()
+        elseif this.queue[1] then
+            this:setup_parse(this.queue[1])
+            table.remove(this.queue, 1)
+            this:open_directory()
         end
-        this:open_directory()
     end,
 
     --queue an item to be opened
@@ -553,7 +550,7 @@ local directory_parser = {
             mp.commandv("script-message", parser.."/browse-dir", directory, "callback/custom-loadlist")
         else
             top.files = scan_directory(directory)
-            this:continue()
+            this:open_directory()
         end
     end,
 
@@ -578,7 +575,7 @@ local directory_parser = {
                     top.pos = i
                     table.insert(this.stack, { pos = 0, directory = directory..files[i].name, files = nil})
                     this:scan_files()
-                    return
+                    if this.parser ~= "file" then return end
                 end
             end
         end

commit f86c9c9985cae1727123da8a65b59e46a3ef772e

custom-dir-loading: reverted bug caused by b2f1222
local files now do the full recursion, but thanks to tail recursion this
is no-longer a huge issue

file-browser.lua
@@ -576,8 +576,7 @@ local directory_parser = {
                 else
                     top.pos = i
                     table.insert(this.stack, { pos = 0, directory = directory..files[i].name, files = nil})
-                    if this.parser ~= "file" then return this:scan_files()
-                    else this:scan_files() end
+                    return this:scan_files()
                 end
             end
         end