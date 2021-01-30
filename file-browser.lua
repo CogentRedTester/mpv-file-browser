ecaa3cd9788fdc9577b0d9668fa3d52fb083ac3c
CogentRedTester <cogent.redtester@outlook.com>
2021-01-30 18:19:57 +1030

    user-input: updated to use external function code

--- file-browser.lua
+++ file-browser.lua
@@ -1141,30 +1141,8 @@ end)
 ------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
 
-local counter = 1
-local function get_user_input(funct, options)
-    local name = mp.get_script_name()
-    options = options or {}
-    options.id = name .. '/' .. (options.id or "")
-    options.text = options.text or (name.." is requesting user input:")
-
-    local response_string = name.."/__user_input_request/"..counter
-    options.response = response_string
-
-    options = utils.format_json(options)
-    if not options then error("table cannot be converted to json string") ; return end
-
-    -- create a callback for user-input to respond to
-    counter = counter + 1
-    mp.register_script_message(response_string, function(response)
-        mp.unregister_script_message(response_string)
-        response = utils.parse_json(response)
-        funct(response.input, response.err)
-    end)
-
-    mp.commandv("script-message-to", "user_input", "request-user-input", options)
-end
+local ui = dofile(mp.command_native({"expand-path", "~~/scripts/user-input-module.lua"}))
 
 mp.add_key_binding("Alt+o", "browse-directory/get-user-input", function()
-    get_user_input(browse_directory, {text = "open directory:"})
+    ui.get_user_input(browse_directory, {text = "[file-browser] open directory:"})
 end)
