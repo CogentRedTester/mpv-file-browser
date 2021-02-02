1fc1e88cdd0b302b8daf5a34cd247f2c3755a362
CogentRedTester <cogent.redtester@outlook.com>
2021-02-02 18:15:21 +1030

    addons: down_dir now prefers absolute paths stored in item.path

--- file-browser.lua
+++ file-browser.lua
@@ -781,7 +781,7 @@ local function down_dir()
     if not state.list[state.selected] or state.list[state.selected].type ~= 'dir' then return end
 
     cache:push()
-    state.directory = state.directory..state.list[state.selected].name
+    state.directory = state.list[state.selected].path or state.directory..state.list[state.selected].name
     update()
 end
 
