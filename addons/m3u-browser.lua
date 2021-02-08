cf4a183960d85f1c772b103bd88211f32857b7d6
CogentRedTester <cogent.redtester@outlook.com>
2021-02-08 22:35:16 +1030

    m3u-browser: add labels and direct paths to playlist files

--- addons/m3u-browser.lua
+++ addons/m3u-browser.lua
@@ -61,7 +61,8 @@ function pl_fixer:parse(directory)
     for _, item in ipairs(list) do
         if exts[ self.get_extension(item.name) ] then
             item.type = "dir"
-            -- item.label = item.name
+            item.path = directory..item.name
+            item.label = item.name
             item.name = item.name.."/"
         end
     end
