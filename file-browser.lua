2ba07c4c7e2b4fe3e45f002796bd667966d26bfc
CogentRedTester <cogent.redtester@outlook.com>
2022-11-04 17:18:45 +1030

    keep empty text as ~ until directory has finished loading

--- file-browser.lua
+++ file-browser.lua
@@ -1098,13 +1098,13 @@ local function update(moving_adjacent)
     state.list = {}
     disable_select_mode()
     update_ass()
-    state.empty_text = "empty directory"
 
     --the directory is always handled within a coroutine to allow addons to
     --pause execution for asynchronous operations
     API.coroutine.run(function()
         state.co = coroutine.running()
         update_list(moving_adjacent)
+        state.empty_text = "empty directory"
         update_ass()
     end)
 end
