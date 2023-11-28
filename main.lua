85f74a0ae22da020228ce6f2fedd423ec129aeab
CogentRedTester <cogent.redtester@outlook.com>
2023-11-28 22:59:51 +1030

    move file parser and root parser to separate modules

--- main.lua
+++ main.lua
@@ -89,64 +89,10 @@ local cache = require 'modules.cache'
 --parser object for the root
 --not inserted to the parser list as it has special behaviour
 --it does get get added to parsers under it's ID to prevent confusing duplicates
-local root_parser = {
-    name = "root",
-    priority = math.huge,
-
-    --if this is being called then all other parsers have failed and we've fallen back to root
-    can_parse = function() return true end,
-
-    --we return the root directory exactly as setup
-    parse = function(self)
-        return root, {
-            sorted = true,
-            filtered = true,
-            escaped = true,
-            parser = self,
-            directory = "",
-        }
-    end
-}
+local root_parser = require 'modules.parsers.root'
 
 --parser ofject for native filesystems
-local file_parser = {
-    name = "file",
-    priority = 110,
-
-    --as the default parser we'll always attempt to use it if all others fail
-    can_parse = function(_, directory) return true end,
-
-    --scans the given directory using the mp.utils.readdir function
-    parse = function(self, directory)
-        local new_list = {}
-        local list1 = utils.readdir(directory, 'dirs')
-        if list1 == nil then return nil end
-
-        --sorts folders and formats them into the list of directories
-        for i=1, #list1 do
-            local item = list1[i]
-
-            --filters hidden dot directories for linux
-            if self.valid_dir(item) then
-                msg.trace(item..'/')
-                table.insert(new_list, {name = item..'/', type = 'dir'})
-            end
-        end
-
-        --appends files to the list of directory items
-        local list2 = utils.readdir(directory, 'files')
-        for i=1, #list2 do
-            local item = list2[i]
-
-            --only adds whitelisted files to the browser
-            if self.valid_file(item) then
-                msg.trace(item)
-                table.insert(new_list, {name = item, type = 'file'})
-            end
-        end
-        return API.sort(new_list), {filtered = true, sorted = true}
-    end
-}
+local file_parser = require 'modules.parsers.file'
 
 
 
