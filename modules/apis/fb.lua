1be395a36ca64f29f7171fa6369bf102b0f58ee2
CogentRedTester <cogent.redtester@outlook.com>
2025-02-22 17:32:50 +1030

    add type annotations for fb and parser APIs

--- modules/apis/fb.lua
+++ modules/apis/fb.lua
@@ -11,6 +11,7 @@ local scanning = require 'modules.navigation.scanning'
 local cache = require 'modules.cache'
 local controls = require 'modules.controls'
 
+---@class FbAPI: FBUtils
 local fb = setmetatable({}, { __index = setmetatable({}, { __index = fb_utils }) })
 package.loaded["file-browser"] = setmetatable({}, { __index = fb })
 
@@ -19,31 +20,44 @@ fb.redraw = ass.update_ass
 fb.rescan = scanning.rescan
 fb.browse_directory = controls.browse_directory
 
+---Clears the directory cache.
 function fb.clear_cache()
     cache:clear()
 end
 
---a wrapper around scan_directory for addon API
+---A wrapper around scan_directory for addon API.
+---@async
+---@param directory string
+---@param parse_state ParseStateTemplate
+---@return Item[]|nil
+---@return Opts
 function fb.parse_directory(directory, parse_state)
     if not parse_state then parse_state = { source = "addon" }
     elseif not parse_state.source then parse_state.source = "addon" end
     return scanning.scan_directory(directory, parse_state)
 end
 
---register file extensions which can be opened by the browser
+---Register file extensions which can be opened by the browser.
+---@param ext string
 function fb.register_parseable_extension(ext)
     g.parseable_extensions[string.lower(ext)] = true
 end
+
+---Deregister file extensions which can be opened by the browser.
+---@param ext string
 function fb.remove_parseable_extension(ext)
     g.parseable_extensions[string.lower(ext)] = nil
 end
 
---add a compatible extension to show through the filter, only applies if run during the setup() method
+---Add a compatible extension to show through the filter, only applies if run during the setup() method.
+---@param ext string
 function fb.add_default_extension(ext)
     table.insert(g.compatible_file_extensions, ext)
 end
 
---add item to root at position pos
+---Add item to root at position pos.
+---@param item Item
+---@param pos? number
 function fb.insert_root_item(item, pos)
     msg.debug("adding item to root", item.label or item.name, pos)
     item.ass = item.ass or fb.ass_escape(item.label or item.name)
@@ -51,7 +65,11 @@ function fb.insert_root_item(item, pos)
     table.insert(g.root, pos or (#g.root + 1), item)
 end
 
--- add a new mapping to the given directory
+---Add a new mapping to the given directory.
+---@param directory string
+---@param mapping string
+---@param pattern? boolean
+---@return string
 function fb.register_directory_mapping(directory, mapping, pattern)
     if not pattern then mapping = '^'..fb_utils.pattern_escape(mapping) end
     g.directory_mappings[mapping] = directory
@@ -61,7 +79,9 @@ function fb.register_directory_mapping(directory, mapping, pattern)
     return mapping
 end
 
--- remove all directory mappings that map to the given directory
+---Remove all directory mappings that map to the given directory.
+---@param directory string
+---@return string[]
 function fb.remove_all_mappings(directory)
     local removed = {}
     for mapping, target in pairs(g.directory_mappings) do
@@ -73,10 +93,12 @@ function fb.remove_all_mappings(directory)
     return removed
 end
 
---a newer API for adding items to the root
---only adds the item if the same item does not already exist in the root
---the priority variable is a number that specifies the insertion location
---a lower priority is placed higher in the list and the default is 100
+---A newer API for adding items to the root.
+---Only adds the item if the same item does not already exist in the root.
+---@param item any
+---@param priority? number Specifies the insertion location, a lower priority
+---                        is placed higher in the list and the default is 100.
+---@return boolean
 function fb.register_root_item(item, priority)
     msg.verbose('registering root item:', utils.to_string(item))
     if type(item) == 'string' then
@@ -88,9 +110,12 @@ function fb.register_root_item(item, priority)
         return fb.get_full_path(r, '') == fb.get_full_path(item, '')
     end) then return false end
 
-    item._priority = priority
+    ---@type table<Item,number>
+    local priorities = {}
+
+    priorities[item] = priority
     for i, v in ipairs(g.root) do
-        if (v._priority or 100) > (priority or 100) then
+        if (priorities[v] or 100) > (priority or 100) then
             fb.insert_root_item(item, i)
             return true
         end
@@ -100,8 +125,13 @@ function fb.register_root_item(item, priority)
 end
 
 --providing getter and setter functions so that addons can't modify things directly
-function fb.get_script_opts() return fb.copy_table(o) end
+
+
+---@param key string
+---@return boolean|string|number
 function fb.get_opt(key) return o[key] end
+
+function fb.get_script_opts() return fb.copy_table(o) end
 function fb.get_extensions() return fb.copy_table(g.extensions) end
 function fb.get_sub_extensions() return fb.copy_table(g.sub_extensions) end
 function fb.get_audio_extensions() return fb.copy_table(g.audio_extensions) end
@@ -119,18 +149,22 @@ function fb.get_selected_item() return fb.copy_table(g.state.list[g.state.select
 function fb.get_open_status() return not g.state.hidden end
 function fb.get_parse_state(co) return g.parse_states[co or coroutine.running() or ""] end
 
--- deprecated
+---@deprecated
+---@return string|nil
 function fb.get_dvd_device()
     local dvd_device = mp.get_property('dvd-device')
-    if not dvd_device then return nil end
+    if not dvd_device or dvd_device == '' then return nil end
     return fb_utils.fix_path(dvd_device, true)
 end
 
+---@param str string
 function fb.set_empty_text(str)
     g.state.empty_text = str
     fb.redraw()
 end
 
+---@param index number
+---@return number|false
 function fb.set_selected_index(index)
     if type(index) ~= "number" then return false end
     if index < 1 then index = 1 end
--- modules/apis/parser.lua
+++ modules/apis/parser.lua
@@ -4,7 +4,7 @@ local g = require 'modules.globals'
 local scanning = require 'modules.navigation.scanning'
 local fb = require 'modules.apis.fb'
 
----@class ParserAPI
+---@class ParserAPI: FbAPI
 local parser_api = setmetatable({}, { __index = fb })
 
 ---Returns the index of the parser.
@@ -15,12 +15,21 @@ function parser_api:get_index() return g.parsers[self].index end
 ---@return string
 function parser_api:get_id() return g.parsers[self].id end
 
---a wrapper that passes the parsers priority value if none other is specified
+---A newer API for adding items to the root.
+---Only adds the item if the same item does not already exist in the root.
+---Wrapper around `fb.register_root_item`.
+---@param item number
+---@param priority? number  The priority for the added item. Uses the parsers priority by default.
+---@return boolean
 function parser_api:register_root_item(item, priority)
     return fb.register_root_item(item, priority or g.parsers[self:get_id()].priority)
 end
 
---runs choose_and_parse starting from the next parser
+---Runs choose_and_parse starting from the next parser.
+---@async
+---@param directory string
+---@return Item[]?
+---@return Opts?
 function parser_api:defer(directory)
     msg.trace("deferring to other parsers...")
     local list, opts = scanning.choose_and_parse(directory, self:get_index() + 1)
